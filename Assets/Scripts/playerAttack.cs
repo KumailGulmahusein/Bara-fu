0b6fe7eac7dd353214516a699e0d67e983a73927
KumailGulmahusein <61838026+KumailGulmahusein@users.noreply.github.com>
2022-11-19 15:43:13 +0000

    Attack almost done

--- Assets/Scripts/playerAttack.cs
+++ Assets/Scripts/playerAttack.cs
@@ -7,6 +7,14 @@ public class playerAttack : MonoBehaviour
 
     Animator myAnim;
 
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public int maxHealth = 100;
+    int currentHealth;
+    public int attackDamage = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +32,26 @@ public class playerAttack : MonoBehaviour
 
     void Attack()
     {
+        //Play animation
         myAnim.SetTrigger("attacking");
+
+        //Detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (attackPoint == null)
+                return;
+
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+
+        }
+
     }
 }
